750abb241067659dd6bf7131a8b0f21ab37b6735
dixma94 <32520039+dixma94@users.noreply.github.com>
2022-07-07 23:48:34 +0600

    IndexFromPosition
    добавил метод поиска индекса ячейки из позиции

--- Assets/Scripts/CellSpawnManager.cs
+++ Assets/Scripts/CellSpawnManager.cs
@@ -13,9 +13,9 @@ public class CellSpawnManager : MonoBehaviour
 
     void Start()
     {
-       
+
         CreateTable(columnsCount, rowsCount);
-        
+
     }
 
     public void CreateTable(int columnsCount, int rowsCount)
@@ -27,15 +27,15 @@ public class CellSpawnManager : MonoBehaviour
             {
                 Vector3 startPos = FindStartPosition(columnsCount, rowsCount);
 
-                var shiftVector = new Vector3(startPos.x + i*(cellMargin+1), startPos.y - j * (cellMargin + 1), 0);
+                var shiftFromStart = new Vector3(startPos.x + i * (cellMargin + 1), startPos.y - j * (cellMargin + 1), 0);
 
-                var newCell = Instantiate(cellPrefab, shiftVector, cellPrefab.transform.rotation);
+                var newCell = Instantiate(cellPrefab, shiftFromStart, cellPrefab.transform.rotation);
 
                 newCell.transform.parent = spawnObject.transform;
                 cellsArray[i, j] = newCell;
             }
         }
-        
+
     }
     public Vector3 FindStartPosition(float columnsCount, float rowsCount)
     {
@@ -44,9 +44,30 @@ public class CellSpawnManager : MonoBehaviour
         return new Vector3(startPosX, startPosY);
     }
 
-    public Vector3 FindCellPosFromIndex(int xIndex, int yIndex)
+    public Vector3 FindCellPosition(int xIndex, int yIndex)
     {
         return cellsArray[xIndex, yIndex].transform.position;
     }
 
+    public (int, int) FindCellIndex(Vector3 cellPosition)
+    {
+        int columnsCount = cellsArray.GetUpperBound(0)+1;
+        int rowsCount = cellsArray.GetUpperBound(1)+1;
+
+        Vector3 startPos = FindStartPosition(columnsCount, rowsCount);
+
+        for (int i = 0; i < columnsCount; i++)
+        {
+            for (int j = 0; j < rowsCount; j++)
+            {
+                var shiftFromStar = new Vector3(startPos.x + i * (cellMargin + 1), startPos.y - j * (cellMargin + 1), 0);
+                if (shiftFromStar == cellPosition)
+                {
+                    return (i, j);
+                }
+                
+            }
+        }
+        return (-1, -1);
+    }
 }
