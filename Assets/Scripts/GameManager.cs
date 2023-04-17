00a9dbf61680b0a413b8448c78b68e4c6001f534
dixma94 <32520039+dixma94@users.noreply.github.com>
2023-04-17 23:51:12 +0600

    удаление астреройдов если они находятся в задании

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -23,7 +24,7 @@ public class GameManager : MonoBehaviour
         //создаем поле для задания
 
         TaskManager.CreateTable(TaskManager.columnsCount, TaskManager.rowsCount);
-        TaskManager.CreateTask(maxlevel, 7);
+        TaskManager.CreateTask(maxlevel, 5);
 
         cellSpawnManager.GetComponent<CellChangePosition>().RefreshTask.AddListener(TakeMove);
         
@@ -42,35 +43,60 @@ public class GameManager : MonoBehaviour
     public bool FindTask(CellManager cellManager, CellManager taskManager)
     {
         if (taskManager == null) return false;
+        List<CellScript> listObstacle = new List<CellScript>();
+        List<CellScript> array = new List<CellScript>();
 
         string task = null;
         string table = null;
-
         int coltask = taskManager.columnsCount;
         int rowtask = taskManager.rowsCount;
         int rowtable = cellManager.rowsCount;
 
+        
+
         foreach (var item in taskManager.cellsArray)
         {
             task += item.Level.ToString();
-
         }
         foreach (var item in cellManager.cellsArray)
         {
-            table += item.Level.ToString();
+            array.Add(item);
         }
- 
-            for (int i = 0; i < (table.Length - rowtable * (coltask - 1) - (coltask-1)); i++)
+
+        for (int i = 0; i < (array.Count - rowtable * (coltask - 1) - (coltask - 1)); i++)
+        {
+            List<CellScript> temp = new List<CellScript>();
+            listObstacle.Clear();
+            table = null;
+            for (int j = 0; j < coltask; j++)
             {
-                string temp = null;
-                for (int j = 0; j < coltask; j++)
-                {
-                    temp += table.Substring((i + rowtable * j), rowtask);
-                }
-                if (temp.Equals(task)) return true;
-
-
+              temp =  temp.Concat(array.Skip((i + rowtable * j)).Take(rowtask)).ToList();
+                
             }
+            foreach(var item in temp)
+            {
+                if (item.Level ==9)
+                {
+                    listObstacle.Add(item);
+                    table += "0";
+                }
+                else
+                {
+                    table += item.Level.ToString();
+                }
+            }
+            if (task.Equals(table))
+            {
+                foreach (var item in listObstacle)
+                {
+                    Destroy(item.ship);
+                    item.ship = null;
+                    item.Level = 0;
+                }
+                return true;
+            }
+        }
+
 
        
         return false;
@@ -82,7 +108,7 @@ public class GameManager : MonoBehaviour
         MovesCounter.CountMoves++;
         if (MovesCounter._CountMoves%8 == 0)
         {
-            cellSpawnManager.AddShip(1, 7);
+            cellSpawnManager.AddShip(1, 9);
         }
         if (FindTask(cellSpawnManager, TaskManager))
         {
