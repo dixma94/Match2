2c42dd0493edbea24bbd9fce05fd579d7b0f6b5d
dixma94 <32520039+dixma94@users.noreply.github.com>
2023-07-04 16:16:38 +0600

    удалил класс rocketDragHandler переместил все методы и поля оттуда в CellManager

--- Assets/Scripts/CellManagers/InventoryCellManager.cs
+++ Assets/Scripts/CellManagers/InventoryCellManager.cs
@@ -1,8 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InventoryCellManager : CellManager
 {
+    [SerializeField]
+    private TableCellManager destinationCellManager;
+    private bool firstCLick = true;
 
+    public int points;
+
+    private InputHandler _inputHandler;
+
+    public UnityEvent TakeMove;
+
+    private Cell firstCell;
+    private Cell secondCell;
+    private void Awake()
+    {
+        _inputHandler = GetComponent<InputHandler>();
+
+    }
+    private void OnEnable()
+    {
+        _inputHandler.ShipUp += PickUpShip;
+    }
+
+    private void OnDisable()
+    {
+        _inputHandler.ShipUp -= PickUpShip;
+
+    }
+     void PickUpShip(Vector2 vector)
+    {
+        if (firstCLick)
+        {
+            if (FindCell(vector, out firstCell))
+            {
+                firstCLick = false;
+            }
+        }
+        else
+        {
+            if (firstCell != null && destinationCellManager.FindCell(vector, out secondCell) && secondCell.ItemType == ItemType.Obstacle)
+            {
+                //разрущаем корабли и создаем новый
+                Destroy(secondCell.item);
+                secondCell.item = null;
+                secondCell.ItemType = ItemType.Empty;
+                secondCell.Level = 0;
+                firstCLick = true;
+            }
+        }
+    }
 }
